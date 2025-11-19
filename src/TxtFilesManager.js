051fe273464e4379f08c7a99daa9c1689745194e
fradiavoloinvoice <fradiavoloinvoice@gmail.com>
2025-11-19 11:36:11 +0100

    Update TxtFilesManager.js

--- src/TxtFilesManager.js
+++ src/TxtFilesManager.js
@@ -14,7 +14,7 @@ import {
   AlertCircle,
   Loader2
 } from 'lucide-react';
-import ErrorsSection from '../../components/ErrorsSection';
+import ErrorsSection from './ErrorsSection';
 
 /**
  * Gestione File TXT
